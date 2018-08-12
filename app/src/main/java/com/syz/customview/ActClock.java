9ce314d69e0ad5ff86e0dd395ec71344a703fa91
shiyuzhe <18091664559@163.com>
2018-08-13 00:09:28 +0800

    Initial commit

---
+++ app/src/main/java/com/syz/customview/ActClock.java
@@ -0,0 +1,13 @@
+package com.syz.customview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActClock extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_clock);
+    }
+}
---
+++ app/src/main/java/com/syz/customview/ActHome.kt
@@ -0,0 +1,12 @@
+package com.syz.customview
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ActHome : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.act_home)
+    }
+}
